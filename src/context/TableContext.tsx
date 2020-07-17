f6c021d9fcecce1537ed2587586bf4ad1e721356
Novosad Konstantin Valentinovich <konstantus@live.ru>
2020-07-18 00:27:18 +0300

    RWT improves common context

---
+++ src/context/TableContext.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { TGrid, TItem } from '../../types';
+
+export interface ITableContext {
+  grid: TGrid;
+  sorting: {
+    sortField: string;
+    sortDirection: 'asc' | 'desc';
+    changeSort(newSortField: string): void;
+    isEmpty: boolean;
+  };
+  classPrefix: string;
+  selectPath(kit: TItem, path: string): (string | number | undefined | null);
+}
+
+export interface ITableContextProps {
+  context: ITableContext;
+}
+
+const defaultValue: ITableContext = {
+  grid: [],
+  sorting: {
+    sortField: '',
+    sortDirection: 'asc',
+    changeSort: (name) => undefined,
+    isEmpty: true,
+  },
+  classPrefix: 'rwt-',
+  selectPath: (kit, path) => kit[path],
+};
+
+export const TableContext = React.createContext<ITableContext>(defaultValue);
+const { Consumer } = TableContext;
+
+export function withTableContext<T>(Comp: React.ComponentType): React.FC<Omit<T, 'context'>> {
+  return function TableContextWrapper(props) {
+    return <Consumer>{(context) => <Comp context={context} {...props} />}</Consumer>;
+  };
+}
